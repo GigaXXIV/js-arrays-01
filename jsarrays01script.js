bc85d1cedef9206701c23fb9c299a99da4d8fffb
Andrew Neil Young <andrewneilyoung88@gmail.com>
2022-12-08 15:31:51 +1100

    All work done.

---
+++ jsarrays01script.js
@@ -0,0 +1,80 @@
+// MOVIE QUOTES
+
+// const quotes = [
+//     "I have the power",
+//     "There can only be one",
+//     "Feel the need. The need for speed."
+// ]
+
+// console.log(quotes[2]);
+// console.log(quotes[quotes.length -1]);
+// console.log(quotes.length);
+
+// quotes[0] = "Power Rangers"
+
+// console.log(quotes);
+
+// for vs for..of
+
+// for
+
+// const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+// for (let i = 0; i < letters.length; i++) {
+//     console.log(i + " - " + letters[i]);
+// }
+
+// // for..of
+
+// for (let element of letters) {
+//     console.log(element);
+// }
+
+// SUM
+
+// const numbers = [23, 19, 23, 52, 12]
+
+// let sum = 0;
+
+// for (let element of numbers) {
+//     sum += element;
+// }
+// console.log(sum);
+
+// WILD WILD LIFE
+
+// // name, species, age, hometown
+// let wolfy = ['Wolfy', 'wolf', 16, 'Yukon Territory']
+// let sharky = ['Sharky', 'shark', 20, 'Left Coast']
+// let plantee = ['Plantee', 'plant',  5000 , 'Mordor']
+// let porgee = ['Porgee', 'Porg', 186, 'Ahch-To']
+// let dart = ["Dart" , 'Demogorgan Dog', 2, 'Upside Down']
+
+// plantee[2] = 5001
+// wolfy[3] = "Gotham City"
+// dart.push("Hawkins")
+// wolfy.splice(0, 1, "Gameboy")
+
+// console.log(wolfy);
+// console.log(plantee);
+// console.log(dart);
+
+// YELL AT THE NINJA TURTLES
+
+let ninjaTurtles = [
+    "Donatello",
+    "Leonardo",
+    "Raphael",
+    "Michaelangelo",
+]
+
+for (let element of ninjaTurtles) {
+    console.log(element.toUpperCase());
+}
+
+
+
+
+
+
+
